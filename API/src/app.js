d63b699fdc2cb5e1ffb758d500df64f691b7d1e7
FFIRELESS <dimatok006@gmail.com>
2021-12-03 17:45:53 +0200

    Added express, dotenv, eslint, husky, lint-staged n prettier

---
+++ API/src/app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {res.send('This is testing api')
+})
+
+console.log(process.env.APP_PORT);
+app.listen(process.env.APP_PORT)

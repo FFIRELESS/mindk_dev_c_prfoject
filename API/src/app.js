b7040f28923b3dfc6f2d73a1f0464fb45c32a34c
FFIRELESS <dimatok006@gmail.com>
2021-12-04 19:42:32 +0200

    Trying conf prettier-plugin

--- API/src/app.js
+++ API/src/app.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', function (req, res) {
-    res.send('This is testing api')
-})
+app.get("/", function (req, res) {
+  res.send("This is testing api");
+});
 
 console.log(process.env.APP_PORT);
 app.listen(process.env.APP_PORT);

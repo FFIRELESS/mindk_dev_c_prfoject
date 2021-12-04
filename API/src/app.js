1cabbbbb15d2fac9819113d8b6ee4ad40548ea20
FFIRELESS <dimatok006@gmail.com>
2021-12-04 17:59:20 +0200

    with pre-hooks error

--- API/src/app.js
+++ API/src/app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {res.send('This is testing api')
+app.get('/', function (req, res) {
+    res.send('This is testing api')
 })
 
 console.log(process.env.APP_PORT);

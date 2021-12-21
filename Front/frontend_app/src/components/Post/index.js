dbf03589527f650e10b9a755281c9188a4cc7d45
FFIRELESS <dimatok006@gmail.com>
2021-12-21 17:16:54 +0200

    little magic in eslint

--- Front/frontend_app/src/components/Post/index.js
+++ Front/frontend_app/src/components/Post/index.js
@@ -1,10 +1,12 @@
-export function Post({ postHeader, postImg, postText, postAuthor }) {
-    return (
-        <div>
-            <p><b>{postHeader}</b></p>
-            <p>{postImg}</p>
-            <p>{postText}</p>
-            <p><i>{postAuthor}</i></p>
-        </div>
-    );
-}
+export var Post = function ({
+  postHeader, postImg, postText, postAuthor,
+}) {
+  return (
+    <div>
+      <p><b>{postHeader}</b></p>
+      <p>{postImg}</p>
+      <p>{postText}</p>
+      <p><i>{postAuthor}</i></p>
+    </div>
+  );
+};
--- Front/frontend_app/src/containers/Post/index.js
+++ Front/frontend_app/src/containers/Post/index.js
@@ -1,9 +1,11 @@
-import {Post} from "../../components/Post";
+import { Post } from '../../components/Post';
 
-export function PostContainer({ postHeader, postImg, postText, postAuthor }) {
-    const header = `${postHeader}!`;
-    const text = "Today " + postText;
-    const author = postAuthor + " //2021"
+export var PostContainer = function ({
+  postHeader, postImg, postText, postAuthor,
+}) {
+  const header = `${postHeader}!`;
+  const text = `Today ${postText}`;
+  const author = `${postAuthor} //2021`;
 
-    return <Post postHeader={header} postImg={postImg} postText={text} postAuthor={author} />
-}
+  return <Post postHeader={header} postImg={postImg} postText={text} postAuthor={author} />;
+};

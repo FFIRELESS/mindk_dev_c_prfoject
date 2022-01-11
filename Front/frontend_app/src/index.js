bd8d744d71984ef04de3fdbdd4fe51e2c6ceac18
FFIRELESS <dimatok006@gmail.com>
2022-01-11 19:45:47 +0200

    added routes and regex validation

--- Front/frontend_app/src/index.js
+++ Front/frontend_app/src/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
 import './index.css';
-import App from './App';
+import App from "./App";
+import {Add_article, Articles, Profile} from "./components/body";
+import {ValidateDate, ValidatePostDigits, ValidatePostFile, ValidatePostUpper} from "./components/Post";
+import {NotFound} from "./components/NotFound";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<App/>}/>
+              <Route path="/profile" element={<Profile/>}/>
+              <Route path="/articles" element={<Articles/>}/>
+              <Route path="/add_article" element={<Add_article/>}/>
+              <Route path="/posts_digits/:id" element={<ValidatePostDigits/>}/>
+              <Route path="/posts_upper/:id" element={<ValidatePostUpper/>}/>
+              <Route path="/posts_file/:id" element={<ValidatePostFile/>}/>
+              <Route path="/date/:date" element={<ValidateDate/>}/>
+              <Route path="*" element={<NotFound/>}/>
+          </Routes>
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );

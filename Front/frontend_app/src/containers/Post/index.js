e77d6df6ed73ea53ade6438a112df97c0eddd328
FFIRELESS <dimatok006@gmail.com>
2022-01-15 16:44:03 +0200

    tried propTypes and ErrorBoundary

--- Front/frontend_app/src/containers/Post/index.js
+++ Front/frontend_app/src/containers/Post/index.js
@@ -1,11 +1,25 @@
 import { Post } from '../../components/Post';
+import PropTypes from "prop-types";
 
-export var PostContainer = function ({
+const PostContainer = ({
   postHeader, postImg, postText, postAuthor,
-}) {
+}) => {
   const header = `${postHeader}!`;
   const text = `Today ${postText}`;
   const author = `${postAuthor} //2021`;
 
   return <Post postHeader={header} postImg={postImg} postText={text} postAuthor={author} />;
 };
+
+PostContainer.propTypes = {
+  postHeader: PropTypes.string.isRequired,
+  postImg: PropTypes.string,
+  postText: PropTypes.string.isRequired,
+  postAuthor: PropTypes.string.isRequired
+}
+
+PostContainer.defaultProps = {
+  postImg: '/src/default.img'
+}
+
+export default PostContainer;
